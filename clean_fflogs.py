1b6384f1188f4e5d726e2caf705fb51825fe78fe
Lawrence Hsia <ray.hsia@gmail.com>
2021-11-14 14:04:35 -0600

    Doing code cleanup

--- clean_fflogs.py
+++ clean_fflogs.py
@@ -2,10 +2,8 @@ import os
 from datetime import datetime
 import glob
 
-
-# this string is the pattern that will retrieve act's fflog txt files.
-# when plugged into glob.glob()
-fflog_regex = os.path.join(os.getenv('APPDATA'), r'Advanced Combat Tracker\FFXIVLogs\*.log')
+# how many days old a file should be before it's deleted.
+DELETION_AGE = 2 
 
 def getFileDate(fileName):
     fileName = fileName.split(".")[0]
@@ -13,15 +11,19 @@ def getFileDate(fileName):
     file_date = parts[len(parts) - 1]
     return datetime.strptime(file_date, "%Y%m%d")
 
+# this string is the pattern that will retrieve act's fflog txt files.
+# when plugged into glob.glob()
+fflog_regex = os.path.join(os.getenv('APPDATA'), r'Advanced Combat Tracker\FFXIVLogs\*.log')
 file_list = glob.glob(fflog_regex)
 
 # this loop deletes anything that's 2 days or older
 for file_name in file_list:
     file_date = getFileDate(file_name)
     todays_date = datetime.today()
-    days_apart = (todays_date - file_date).days
+    # how many days old the file is
+    file_age = (todays_date - file_date).days 
 
-    if days_apart > 2:
+    if file_age > DELETION_AGE:
         print(f'deleting file: {file_name}')
         os.remove(file_name)
     else:
