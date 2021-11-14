37f50e6a11e66fe5cf1e8bd1c97577d66fe95c7a
Lawrence Hsia <ray.hsia@gmail.com>
2021-11-14 14:42:29 -0600

    added error handling for log files

--- clean_fflogs.py
+++ clean_fflogs.py
@@ -9,16 +9,22 @@ def getFileDate(fileName):
     fileName = fileName.split(".")[0]
     parts = fileName.split("_")
     file_date = parts[len(parts) - 1]
-    return datetime.strptime(file_date, "%Y%m%d")
+    try:
+        return datetime.strptime(file_date, "%Y%m%d")
+    except ValueError:
+        return None
 
 # this string is the pattern that will retrieve act's fflog txt files.
 # when plugged into glob.glob()
-fflog_regex = os.path.join(os.getenv('APPDATA'), r'Advanced Combat Tracker\FFXIVLogs\*.log')
+fflog_regex = os.path.join(os.getenv('APPDATA'), r'Advanced Combat Tracker\FFXIVLogs\Network_*.log')
 file_list = glob.glob(fflog_regex)
 
 # this loop deletes anything that's 2 days or older
 for file_name in file_list:
     file_date = getFileDate(file_name)
+    if( file_date is None ):
+        print(f"{file_name} is not an ACT log file. Skipping...")
+        continue
     todays_date = datetime.today()
     # how many days old the file is
     file_age = (todays_date - file_date).days 
